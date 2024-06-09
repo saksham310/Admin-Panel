84f2a74a1adc1e7dd33c53706dc8c74a2fa8e734
saksham.s <sakshams982@gmail.com>
2024-06-09 14:23:04 +0545

    created a form to add new products

--- Admin-Panel/src/app/form/product-form/product-form.component.ts
+++ Admin-Panel/src/app/form/product-form/product-form.component.ts
@@ -1,19 +1,29 @@
-import { Component, inject } from '@angular/core';
+import { Component, ViewEncapsulation, inject } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { DataService } from '../../services/data.service';
 import { Subscription } from 'rxjs';
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+import { FloatLabelModule } from 'primeng/floatlabel';
+import { InputGroupModule } from 'primeng/inputgroup';
+import { InputGroupAddonModule } from 'primeng/inputgroupaddon';
+import { InputTextModule } from 'primeng/inputtext';
+import { DropdownModule } from 'primeng/dropdown';
 
 @Component({
   selector: 'app-product-form',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule,CardModule,
+    DropdownModule, ButtonModule,FloatLabelModule,InputTextModule,InputGroupModule,InputGroupAddonModule], 
   templateUrl: './product-form.component.html',
-  styleUrl: './product-form.component.css'
+  styleUrl: './product-form.component.css',
+  encapsulation:ViewEncapsulation.None
 })
 export class ProductFormComponent {
   private subscritption:Subscription=new Subscription();
 private _fb:FormBuilder=inject(FormBuilder);
 form!:FormGroup;
+options:string[]=['In Stock','Out of Stock'];
 constructor(private dataService:DataService){
   this.form=this._fb.group({
     name:[''],
@@ -24,6 +34,12 @@ constructor(private dataService:DataService){
 }
 onSubmit(){
   console.log(this.form.getRawValue());
+  const status=this.form.get("status")?.value;
+  if(status==="In Stock"){
+    this.form.get("status")?.setValue(true);
+  }else{
+    this.form.get("status")?.setValue(false);
+  }
   const add=this.dataService.addProduct(this.form.getRawValue()).subscribe((res)=>{
     console.log(res);
     this.form.reset();
@@ -34,5 +50,6 @@ onSubmit(){
 ngOnDestroy(){
   this.subscritption.unsubscribe();
 }
+
 }
 
